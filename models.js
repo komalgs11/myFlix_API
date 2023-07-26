a11a46b3bbb6410973cae8e0a95d77f3b63334e9
komalgs11 <komalpatil26.kp@gmail.com>
2023-07-26 17:21:13 +0200

    updated models.js

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 
 let movieSchema = mongoose.Schema({
@@ -18,25 +18,25 @@ let movieSchema = mongoose.Schema({
     Featured: Boolean
   });
   
-  let userSchema = mongoose.Schema({
+let userSchema = mongoose.Schema({
     Username: {type: String, required: true},
     Password: {type: String, required: true},
     Email: {type: String, required: true},
     BirthDate: Date,
     favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
-  });
+});
 
-  userSchema.statics.hashPassword = (password) => {
-    return bcrypt.hashSync(password, 10);
-};
+// userSchema.statics.hashPassword = (password) => {
+//   return bcrypt.hashSync(password, 10);
+// };
 
-  userSchema.methods.validatePassword = function(password) {
-    return bcrypt.compareSync(password, this.Password);
-};
+// userSchema.methods.validatePassword = function(password) {
+//   return bcrypt.compareSync(password, this.Password);
+// };
 
   
-  let Movie = mongoose.model('Movie', movieSchema);
-  let User = mongoose.model('User', userSchema);
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
   
-  module.exports.Movie = Movie;
-  module.exports.User = User;
+module.exports.Movie = Movie;
+module.exports.User = User;
