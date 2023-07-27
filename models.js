c4c39e7f3c615bcf17517f45ce052dbea815d3e4
komalgs11 <komalpatil26.kp@gmail.com>
2023-07-27 17:40:44 +0200

    added bcrypt in models.js

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 
 let movieSchema = mongoose.Schema({
@@ -26,13 +26,13 @@ let userSchema = mongoose.Schema({
     favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
-// userSchema.statics.hashPassword = (password) => {
-//   return bcrypt.hashSync(password, 10);
-// };
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
 
-// userSchema.methods.validatePassword = function(password) {
-//   return bcrypt.compareSync(password, this.Password);
-// };
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
   
 let Movie = mongoose.model('Movie', movieSchema);
